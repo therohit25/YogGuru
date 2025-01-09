0a4be2d24a9bd9ca676fff11c23af71ba10619ec
Rohit Ghadage <rohitghadage40@gmail.com>
2025-01-09 13:07:53 +0530

    case-fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import Routes from "./Routes/route";
+import NaviagationRoutes from "./routes/route";
 
 function App() {
   axios.defaults.withCredentials = true;
 
   return (
     <div style={{ width: "100vw" }}>
-      <Routes />
+      <NaviagationRoutes />
     </div>
   );
 }
