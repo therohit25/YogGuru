a73e44d41bc4019b42bdd880456199a88b83a016
Rohit Ghadage <rohitghadage40@gmail.com>
2025-01-09 12:42:48 +0530

    added-cloud-db

--- server/controllers/Product_Controllers/products.js
+++ server/controllers/Product_Controllers/products.js
@@ -1,6 +1,9 @@
-
 const { ProductModel } = require("../../models/ProductModel");
 
 module.exports = (req, res) => {
-    ProductModel.find({}).then((result) => { res.json(result).status(200) }).catch((err) => res.json({ "Error": err }).status(401))
-}
+  ProductModel.find({})
+    .then((result) => {
+      res.json(result).status(200);
+    })
+    .catch((err) => res.json({ Error: err }).status(401));
+};
--- server/data/dbconfig.js
+++ server/data/dbconfig.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 require("dotenv").config({
   path: "./data/config.env",
 });
-mongoose.connect(process.env.MONGO_URI);
+try {
+  mongoose.connect(process.env.MONGO_URI);
+} catch (error) {
+  console.error(error);
+}
 
 module.exports = mongoose;
